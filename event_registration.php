eebc1a2e38a68f933a635221c52ec1d6af4fea00
Andy Click <andyclick@gmail.com>
2013-05-21 11:37:57 -0300

    Update event_registration.php

--- event_registration.php
+++ event_registration.php
@@ -1,1 +1,16 @@
-ph
+<form method="POST" action="transactions/event_name.php">
+<!-- 
+Below is a sample form with personal data only, since each form has very different inputs otherwise.
+Be sure to use <label> tags for accessibility, as it helps those with screen-readers navigate the form.
+Most of the look of form elements is controlled in CSS at the core level.
+//-->
+<p><label for="First_Name">First Name:</label><br />
+<input type="text" name="First_Name" /></p>
+<p><label for="Last_Name">Laste Name:</label><br />
+<input type="text" name="Laste_Name" /></p>
+<p><label for="Email">Email Address:</label><br />
+<input type="text" name="Email" /></p>
+<p><label for="Phone">Phone Number:</label><br />
+<input type="text" name="Phone" /></p>
+<input type="Submit" value="Register" />
+</form>
